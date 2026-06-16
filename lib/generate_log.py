6778b506d1731f7526645e8369476896005d8048
cynthia <njugunacynthia32@gmail.com>
2026-06-16 08:37:59 +0300

    add lib file

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -5,12 +5,11 @@ def generate_log(log_data):
     if not isinstance(log_data, list):
         raise ValueError("Input must be a list")
 
-    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
-
+    today = datetime.now().strftime("%Y%m%d")
+    filename = f"log_{today}.txt"
 
     with open(filename, "w") as file:
         for item in log_data:
-            file.write(f"{item}\n")
+            file.write(item + "\n")
 
     return filename
-    
