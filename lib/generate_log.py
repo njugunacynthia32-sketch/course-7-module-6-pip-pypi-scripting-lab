d98f10c0af8b9028eacb580466d585b4076ee2c2
cynthia <njugunacynthia32@gmail.com>
2026-06-16 10:30:15 +0300

    complete pip pypi scripting lab

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,4 +1,11 @@
 from datetime import datetime
+import requests
+
+def fetch_data():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    if response.status_code == 200:
+        return response.json()
+    return {}
 
 def generate_log(log_data):
     if not isinstance(log_data, list):
@@ -12,5 +19,14 @@ def generate_log(log_data):
             file.write(item + "\n")
 
     print(f"Log file created: {filename}")
-
     return filename
+
+
+if __name__ == "__main__":
+    log_data = ["User logged in", "User updated profile", "Report exported"]
+
+    filename = generate_log(log_data)
+
+    post = fetch_data()
+    print("Fetched Post Title:", post.get("title", "No title found"))
+    
